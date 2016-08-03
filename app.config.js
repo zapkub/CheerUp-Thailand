14fc781eefdff72fbaa7b6a8ed6a1616b291a618
Rungsikorn Rungsikavarnich <rungsikorn@me.com>
2016-08-03 14:50:02 +0700

    เพิ่ม Share description + Ready to pre release + Add travis slack notification

--- app.config.js
+++ app.config.js
@@ -1,21 +1,24 @@
 var shareInfo = {
   link:'http://thesecretfarm.com/cheerup/',
   name: 'CheerUp Thailand',
-  description: 'ยังไม่มี',
-  title: 'ใครทำอะไร',
-  caption: 'เชียร์จ้า',
+  description: 'ร่วมเป็น 1 ในล้านแรงใจ เป็นแรงเชียร์ให้นักกีฬาไทย คว้าชัยโอลิมปิก',
+  title: 'CheerUp Thailand',
+  caption: 'www.cheerthai.co',
 }
-
+// Text1 : เชียร์ไทยสุดใจ ไกลถึงริโอ
+// Text2 : ร่วมเป็น 1 ในล้านแรงใจ เป็นแรงเชียร์ให้นักกีฬาไทย คว้าชัยโอลิมปิก
+// Text3 : www.cheerthai.co
+// ลิ้งค์ด้านบน (ชื่อเกม) : cheerthaisudjai
 if (process.env.PRODUCTION) {
   module.exports = {
-    name: "Cheer Up",
+    name: "เชียร์ไทยสุดใจ ไกลถึงริโอ",
     fbAppId: "667832946653119",
     redirectURL: 'http://www.cheerthai.co/facebook/cheerthaibanner/',
     shareInfo
   }
 }else {
    module.exports = {
-    name: "Cheer Up",
+    name: "เชียร์ไทยสุดใจ ไกลถึงริโอ",
     fbAppId: "1051893558178674",
     redirectURL: 'http://localhost:8080/',
     shareInfo
